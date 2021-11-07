6df16148845ae1c785fc35ce5a24caf4cbb9ff35
Giedrius <kalniusg@gmail.com>
2021-11-07 22:30:11 +0200

    Added few changes

--- src/app/weathers/weathers.component.ts
+++ src/app/weathers/weathers.component.ts
@@ -7,30 +7,21 @@ import { GetWeatherService } from '../services/get-weather.service';
   styleUrls: ['./weathers.component.css'],
 })
 export class WeathersComponent implements OnInit, OnDestroy {
-  public clicked = true;
+  public clicked = false;
   public weathers;
 
   constructor(private getWeatherService: GetWeatherService) {
     this.getWeatherService.buttonClick.subscribe((e) => {
-      console.log(e);
       this.getWeatherService.getWeather(e).subscribe((data) => {
         this.weathers = [data];
         this.clicked = true;
-        // console.log(this.weathers);
       });
     });
   }
 
-  ngOnInit(): void {
-    // this.getWeatherService.getWeather('siauliai').subscribe((data) => {
-    //   this.weathers = [data];
-    //   this.clicked = true;
-    //   // console.log(this.weathers);
-    // });
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy() {
-    // cia kol programa stebi kol programa gauna tai tai pvz destroyinti turime visada
     this.getWeatherService.buttonClick.unsubscribe();
   }
 }
